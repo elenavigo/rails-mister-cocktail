5d26364db051a7ea8d9214c7df8e8476ab6a8484
Elena Vigo <elenavigoolivan@gmail.com>
2020-03-13 16:08:53 +0100

    everything up to date from yesterday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+p 'starting seeds'
+
+Ingredient.destroy_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json_string = open(url).read
+ruby_hash = JSON.parse(json_string)
+
+ruby_hash['drinks'].each do |hash|
+  Ingredient.create!(name: hash['strIngredient1'])
+end
+
+p 'seeds done'
